7f92d2a2960d8a19f6ef8ebb5a8b848f95b29d31
guganravi06 <guganravi06@gmail.com>
2025-03-06 23:32:12 +0530

    completed Signup page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import Button from './components/Button';
-import LeftSideIllustrator from './components/LeftSideIllustrator';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    //adding style just to display button on UI
     <div style={{display:'flex'}}>
-      <LeftSideIllustrator />
-      <Button text="Login" icon='./assets/google-logo-icon.png' textAlign='center'/>
+      <SignUp />
     </div>
   );
 }
 
-export default App;
+export default App; 
