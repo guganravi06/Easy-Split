21abe6bdee4a165760ab7f28f2f704aea37e9e7b
guganravi06 <guganravi06@gmail.com>
2025-03-06 16:23:41 +0530

    created button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Button from './components/Button';
 import LeftSideIllustrator from './components/LeftSideIllustrator';
 
 function App() {
   return (
-    <div>
+    //adding style just to display button on UI
+    <div style={{display:'flex'}}>
       <LeftSideIllustrator />
+      <Button text="Login" icon='./assets/google-logo-icon.png' textAlign='center'/>
     </div>
   );
 }
