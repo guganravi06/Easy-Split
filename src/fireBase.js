0ed66768ed407dc5cd8ec120d861ddd49c42e493
guganravi06 <guganravi06@gmail.com>
2025-03-18 00:01:15 +0530

    Implemenetd SignUp, added producted route , added NotFound Page , update user name dynamically

--- src/fireBase.js
+++ src/fireBase.js
@@ -4,14 +4,23 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDbY7i11-cUTH_65QbwxPQ5orzB_nBLGhQ',
+//   authDomain: 'fir-app-2e139.firebaseapp.com',
+//   projectId: 'fir-app-2e139',
+//   storageBucket: 'fir-app-2e139.firebasestorage.app',
+//   messagingSenderId: '632005998830',
+//   appId: '1:632005998830:web:eb0c9e5eea01e2629e9bff',
+//   measurementId: 'G-BNZ173Q4LX',
+// };
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDbY7i11-cUTH_65QbwxPQ5orzB_nBLGhQ',
-  authDomain: 'fir-app-2e139.firebaseapp.com',
-  projectId: 'fir-app-2e139',
-  storageBucket: 'fir-app-2e139.firebasestorage.app',
-  messagingSenderId: '632005998830',
-  appId: '1:632005998830:web:eb0c9e5eea01e2629e9bff',
-  measurementId: 'G-BNZ173Q4LX',
+  apiKey: 'AIzaSyCSq2xTC_ZgDkaL2zFG_TDeYb6xsolA2os',
+  authDomain: 'easy-split-2a48b.firebaseapp.com',
+  projectId: 'easy-split-2a48b',
+  storageBucket: 'easy-split-2a48b.firebasestorage.app',
+  messagingSenderId: '290465597940',
+  appId: '1:290465597940:web:632149df79282f3c3c51a1',
 };
 
 // Initialize Firebase
@@ -21,11 +30,4 @@ const googleProvider = new GoogleAuthProvider();
 
 export { app, auth, googleProvider };
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCSq2xTC_ZgDkaL2zFG_TDeYb6xsolA2os',
-//   authDomain: 'easy-split-2a48b.firebaseapp.com',
-//   projectId: 'easy-split-2a48b',
-//   storageBucket: 'easy-split-2a48b.firebasestorage.app',
-//   messagingSenderId: '290465597940',
-//   appId: '1:290465597940:web:632149df79282f3c3c51a1',
-// };
+
